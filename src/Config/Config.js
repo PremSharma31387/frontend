101e0f348ebc556cbbdbff82c67c299c33ae4411
Prem Sharma <arya31387@gmail.com>
2024-08-15 23:07:01 +0530

    add

--- src/Config/Config.js
+++ src/Config/Config.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 //export const API_BASE_URL = "http://localhost:8080";
-export const API_BASE_URL = "https://backend-nyds.onrender.com";
+export const API_BASE_URL = "https://ecommercebackend-zprr.onrender.com";
 
 const token = localStorage.getItem("token");
 
